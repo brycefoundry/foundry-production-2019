9821fb9218ba7b06c5c64aa697d49896ae2e88ba
Bryce <bryce@exostudio.com>
2019-05-21 10:49:16 -0500

    Added Usersnap

--- wp-content/themes/foundry-2019/footer.php
+++ wp-content/themes/foundry-2019/footer.php
@@ -69,6 +69,10 @@
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+		<script type="text/javascript">
+		(function() { var s = document.createElement("script"); s.type = "text/javascript"; s.async = true; s.src = '//api.usersnap.com/load/91d9e129-9f29-42f6-b25f-5477e5df1b13.js';
+		var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); })();
+		</script>
 		
 	</body>
 </html>
