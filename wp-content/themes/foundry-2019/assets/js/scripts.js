d37371ec10b185475bb20bd51d3de34be77cabbd
Bryce <bryce@exostudio.com>
2019-06-11 16:09:38 -0500

    homepage created and updated

--- wp-content/themes/foundry-2019/assets/js/scripts.js
+++ wp-content/themes/foundry-2019/assets/js/scripts.js
@@ -24,6 +24,11 @@
 			}
 
 
+			if ($("body").hasClass("home")) {
+				home();
+			}
+			
+
 
 			if($('#password-form').length){
 				$('.container.locked').addClass('show');
@@ -84,6 +89,45 @@
 
 		videocover = function(){
 
+		}
+
+		home = function(){
+			$('.logo-slider').slick({
+			  slidesToShow: 4,
+			  slidesToScroll: 4,
+			  dots: true,
+			  autoplay: true,
+			  autoplaySpeed: 2000,
+			   responsive: [
+			      {
+			        breakpoint: 1024,
+			        settings: {
+			          slidesToShow: 3,
+			          slidesToScroll: 3,
+			          infinite: true,
+			          dots: true
+			        }
+			      },
+			      {
+			        breakpoint: 600,
+			        settings: {
+			          slidesToShow: 2,
+			          slidesToScroll: 2
+			        }
+			      },
+			      {
+			        breakpoint: 480,
+			        settings: {
+			          slidesToShow: 1,
+			          slidesToScroll: 1
+			        }
+			      }
+			      // You can unslick at a given breakpoint now by adding:
+			      // settings: "unslick"
+			      // instead of a settings object
+			    ]
+			  
+			});
 		}
 
 
